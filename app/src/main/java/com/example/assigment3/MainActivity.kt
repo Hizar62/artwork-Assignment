0c5f47bf5386930b6d741d109af36f9c6178819b
Hiza62 <hizarsajjad632@gmail.com>
2023-07-10 12:53:07 +0500

    updating code

--- app/src/main/java/com/example/assigment3/MainActivity.kt
+++ app/src/main/java/com/example/assigment3/MainActivity.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.assigment3.ui.theme.Assigment3Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +51,7 @@ fun ArtWork( modifier: Modifier = Modifier) {
 //        Image
 
             Image(painter = painterResource(id = R.drawable.milky), contentDescription = "Milky way",
-            )
+            modifier = Modifier.fillMaxWidth())
 
 //        Description
         Column(modifier = Modifier.fillMaxWidth(),
@@ -55,12 +62,13 @@ fun ArtWork( modifier: Modifier = Modifier) {
 
 //        Buttons
         Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
             ) {
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp).clip(shape = RoundedCornerShape(8.dp))) {
                 Text(text = "Previous")
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Next")
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp).clip(shape = RoundedCornerShape(8.dp))) {
+                Text(text = "  Next  ")
             }
         }
 
